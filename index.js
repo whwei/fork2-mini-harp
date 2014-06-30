b16775e55739d13b408751d0891b6787dd5d0974
whwei <whw.0922@gmail.com>
2014-06-30 21:20:09 +0800

    add currrent time middleware

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var connect = require('connect');
 
 module.exports = function() {
-  return connect();
+  return connect().use(function(req, res, next) {
+    console.log(req.url)
+    if (req.url === '/current-time') {
+      res.end((new Date).toISOString());
+    } else {
+      res.end('Cannot Get ' + req.url);
+    }
+  });
 };
